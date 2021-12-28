3b732c691c0dd8485f84742a0d07b7bf898e142f
dtran <dtran13899@gmail.com>
2021-12-28 05:10:17 -0600

    Using 2 dices instead of 1. Display message if two 1s are rolled

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 'use strict';
 
 // Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player-0');
+const player1El = document.querySelector('.player-1');
+const score0El = document.querySelector('#score-0');
+const score1El = document.querySelector('#score-1');
+const messageEl = document.querySelectorAll('.message');
+const current0El = document.querySelector('#current-0');
+const current1El = document.querySelector('#current-1');
+const diceEl = document.querySelectorAll('.dice');
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
 
 let scores, currentScore, activePlayer, playing;
 
@@ -26,41 +27,68 @@ const init = function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  diceEl.forEach(el => el.classList.add('hidden'));
+  messageEl.forEach(el => el.classList.add('hidden'));
+  player0El.classList.remove('player-winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player-active');
 };
 
 init();
 
+const setCurScore = function (score) {
+  document.getElementById(`current-${activePlayer}`).textContent = score;
+};
+
+const setTotalScore = function (score) {
+  document.getElementById(`score-${activePlayer}`).textContent = score;
+};
+
 const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  setCurScore(0);
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  document.querySelector(`#message-${activePlayer}`).classList.add('hidden');
+  player0El.classList.toggle('player-active');
+  player1El.classList.toggle('player-active');
 };
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
-    const dice = Math.ceil(Math.random() * 6);
+    const dice0 = Math.ceil(Math.random() * 6);
+    const dice1 = Math.ceil(Math.random() * 6);
+    // const dice0 = 1;
+    // const dice1 = 1;
 
     // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.forEach(el => el.classList.remove('hidden'));
+    diceEl[0].src = `dice-${dice0}.png`;
+    diceEl[1].src = `dice-${dice1}.png`;
 
-    // 3. Check for rolled 1: if true, switch to next player
-    if (dice !== 1) {
+    // 3. Check for rolled 1: if any dice equals 1, switch to next player
+    if (dice0 !== 1 && dice1 !== 1) {
       // Add dice to the current score
-      currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
-    } else {
+      const totalRoll = dice0 + dice1;
+      currentScore += totalRoll;
+
+      // Render current score of that turn
+      setCurScore(currentScore);
+    } else if (dice0 === 1 && dice1 === 1) {
+      // The player’s entire score is lost
+      scores[activePlayer] = 0;
+
+      // Render total score to 0 and display message
+      setTotalScore(0);
+      document
+        .querySelector(`#message-${activePlayer}`)
+        .classList.remove('hidden');
+
       // Switch to next player
+      switchPlayer();
+    } else {
       switchPlayer();
     }
   }
@@ -70,20 +98,19 @@ btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+    setTotalScore(scores[activePlayer]);
 
-    // 2. Check if player's score is >=100
-    if (scores[activePlayer] >= 100) {
+    // 2. Check if player's score is >=200
+    if (scores[activePlayer] >= 200) {
       // Finish the game
       playing = false;
-      diceEl.classList.add('hidden');
+      diceEl.forEach(el => el.classList.add('hidden'));
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .querySelector(`.player-${activePlayer}`)
+        .classList.add('player-winner');
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .querySelector(`.player-${activePlayer}`)
+        .classList.remove('player-active');
     } else {
       // Switch to the next player
       switchPlayer();
